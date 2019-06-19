2806a4f29f9ff9b28faa3b23bf38c629f7798f07
Matteo Baglini <matteo.baglini@gmail.com>
2019-06-19 16:47:05 +0200

    do not include joke routes and logic

--- src/main/scala/com/ynap/mru/MruServer.scala
+++ src/main/scala/com/ynap/mru/MruServer.scala
@@ -1,36 +1,19 @@
 package com.ynap.mru
 
 import cats.effect._
-import cats.implicits._
 import fs2.Stream
-import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
-import fs2.Stream
-
-import scala.concurrent.ExecutionContext.global
 
 object MruServer {
 
   def stream(implicit T: Timer[IO], C: ContextShift[IO]): Stream[IO, Nothing] = {
     for {
-      client <- BlazeClientBuilder[IO](global).stream
+      _ <- Stream.eval(IO.unit)
       helloWorldAlg = HelloWorld.impl[IO]
-      jokeAlg = Jokes.impl[IO](client)
-
-      // Combine Service Routes into an HttpApp.
-      // Can also be done via a Router if you
-      // want to extract a segments not checked
-      // in the underlying routes.
-      httpApp = (
-        MruRoutes.helloWorldRoutes[IO](helloWorldAlg) <+>
-        MruRoutes.jokeRoutes[IO](jokeAlg)
-      ).orNotFound
-
-      // With Middlewares in place
+      httpApp = MruRoutes.helloWorldRoutes[IO](helloWorldAlg).orNotFound
       finalHttpApp = Logger.httpApp(true, true)(httpApp)
-
       exitCode <- BlazeServerBuilder[IO]
         .bindHttp(8080, "0.0.0.0")
         .withHttpApp(finalHttpApp)
